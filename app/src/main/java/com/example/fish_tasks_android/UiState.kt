60b80c62dadf623c5bd731dba016b0c3a90cf0e7
EmilianoMesa <16136387+EmilianoMesa@users.noreply.github.com>
2026-04-28 13:22:11 -0500

    Initial commit

---
+++ app/src/main/java/com/example/fish_tasks_android/UiState.kt
@@ -0,0 +1,9 @@
+package com.example.fish_tasks_android
+
+sealed interface UiState {
+
+    data object Initial : UiState
+    data object Loading : UiState
+    data class Success(val outputText: String) : UiState
+    data class Error(val errorMessage: String) : UiState
+}
